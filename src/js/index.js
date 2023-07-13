014468433f316e5520123446aaf6015536058428
htmljscs <138679363+htmljscs@users.noreply.github.com>
2023-07-13 10:46:31 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const personagens = document.querySelectorAll('.personagem');
+// document.getElementById("coclope")
+// const itemCiclope = document..getElementById("coclope");
+personagens.forEach(personagem => {personagem.addEventListener    ('mouseenter', () => {
+     const personagemSelecionado = document.querySelector('.selecionado');
+     personagemSelecionado.classList.remove('selecionado')
+    personagem.classList.add('selecionado');
+    
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');   
+    const idPersonagem = personagem.attributes.id.value;   
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+   
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+  })
+})
